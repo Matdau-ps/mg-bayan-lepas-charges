b6ea6496b934c0b263a8408d464432a15cca436a
Matdau-ps <er.nurse.ms@gmail.com>
2026-09-11 18:06:28 +0800

    g

--- mobile/sw.js
+++ mobile/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mg-charges-mobile-v7';
+const CACHE = 'mg-charges-mobile-v8';
 const SHELL = ['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png'];
 
 self.addEventListener('install', event => {
